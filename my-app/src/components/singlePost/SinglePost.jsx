eb4c65469aa8bbdb73bfba878f77c7604d751b85
Keely Bailey <keelybailey3@gmail.com>
2022-02-02 21:48:58 -0600

    Practice react

--- my-app/src/components/singlePost/SinglePost.jsx
+++ my-app/src/components/singlePost/SinglePost.jsx
@@ -9,7 +9,21 @@ export default function SinglePost() {
                     alt="random"
                     className="singlePostImg"
                 />
-                <h1 className="singlePostTitle">Lorem ipsum dolor sit amet</h1>
+                <h1 className="singlePostTitle">
+                    Lorem ipsum dolor sit amet
+                
+                <div className="singlePostEdit">
+                    <i class="singlePostIcon far fa-edit"></i>
+                    <i class="singlePostIcon far fa-trash-alt"></i>
+                </div>
+                </h1>
+                <div className="singlePostInfo">
+                    <span className="singlePostAuthor">Author: <b>Keely</b></span>
+                    <span className="singlePostDate">1 hour ago</span>
+                </div>
+                <p className="singlePostDesc">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                </p>
             </div>
         </div>
     )
