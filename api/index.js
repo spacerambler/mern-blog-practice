2fd7b9dd0af165c3334e442d5a7eee242b2a3d6b
Keely Bailey <keelybailey3@gmail.com>
2022-02-06 21:44:58 -0600

    backend

--- api/index.js
+++ api/index.js
@@ -2,8 +2,11 @@ const express = require("express")
 const app = express()
 const dotenv = require("dotenv")
 const mongoose = require("mongoose")
+const authRoute = require("./routes/auth")
 
 dotenv.config()
+
+app.use(express.json())
 
 mongoose
     .connect(process.env.MONGO_URL, {
@@ -12,11 +15,8 @@ mongoose
     .then(console.log("Connected to MongoDB"))
     .catch((err)=>console.log(err))
 
-// app.use("/", (req, res)=>{
-//     console.log("main url")
-// })
+app.use("/api/auth", authRoute)
 
 app.listen("5000", ()=>{
     console.log("Backend is running")
 })
-
