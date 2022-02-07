19404383a4286c080afd0cd84a453efdf351e308
Keely Bailey <keelybailey3@gmail.com>
2022-02-06 20:43:41 -0600

    Backend practice

--- api/index.js
+++ api/index.js
@@ -1,9 +1,20 @@
 const express = require("express")
 const app = express()
+const dotenv = require("dotenv")
+const mongoose = require("mongoose")
 
-app.use("/", (req, res)=>{
-    console.log("main url")
-})
+dotenv.config()
+
+mongoose
+    .connect(process.env.MONGO_URL, {
+        useNewUrlParser: true,
+    })
+    .then(console.log("Connected to MongoDB"))
+    .catch((err)=>console.log(err))
+
+// app.use("/", (req, res)=>{
+//     console.log("main url")
+// })
 
 app.listen("5000", ()=>{
     console.log("Backend is running")
