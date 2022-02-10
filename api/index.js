9ff20e0be124811ccaa99d711aa8ca1b16246046
Keely Bailey <keelybailey3@gmail.com>
2022-02-09 20:54:25 -0600

    Fix routes error

--- api/index.js
+++ api/index.js
@@ -3,11 +3,11 @@ const app = express()
 const dotenv = require("dotenv")
 const mongoose = require("mongoose")
 const authRoute = require("./routes/auth")
-const userRoute = require("./routes/users")
-const postRoute = require("./routes/posts")
+const usersRoute = require("./routes/users")
 
 dotenv.config()
-app.use(express.json)
+
+app.use(express.json())
 
 mongoose
     .connect(process.env.MONGO_URL, {
@@ -16,9 +16,11 @@ mongoose
     .then(console.log("Connected to MongoDB"))
     .catch((err)=>console.log(err))
 
+// app.use("/", (req, res)=>{
+//     console.log("main url")
+// })
 app.use("/api/auth", authRoute)
-app.use("/api/users", userRoute)
-app.use("/api/posts", postRoute)
+app.use("/api/users", usersRoute)
 
 app.listen("5000", ()=>{
     console.log("Backend is running")
