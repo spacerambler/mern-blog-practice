357cd2152f0dc1a1a919a3f0f387a0be28083c8d
Keely Bailey <keelybailey3@gmail.com>
2022-02-10 20:27:18 -0600

    backend

--- api/routes/posts.js
+++ api/routes/posts.js
@@ -14,6 +14,24 @@ router.post("/", async (req, res)=> {
 })
 
 // UPDATE
+router.put("/:id", async (req,res) =>{
+    if (req.body.userId === req.params.id){
+        if (req.body.password){
+            const salt = await bcrypt.genSalt(10)
+            req.body.password = await bcrypt.hash(req.body.password, salt)
+        }
+        try {
+            const updatedUser = await User.findByIdAndUpdate(req.params.id, {
+                $set:req.body,
+            })
+            res.status(200).json(updatedUser)
+        } catch(err) {
+            res.status(500).json(err)
+        }
+    } else {
+        res.status(401).json("Not your account")
+    }
+})
 
 // DELETE
 
