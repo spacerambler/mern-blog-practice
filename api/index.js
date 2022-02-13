72b66298ae02d685719b451431cffbcac0c9a06a
Keely Bailey <keelybailey3@gmail.com>
2022-02-13 15:08:43 -0600

    Category route & image upload route

--- api/index.js
+++ api/index.js
@@ -5,6 +5,8 @@ const mongoose = require("mongoose")
 const authRoute = require("./routes/auth")
 const usersRoute = require("./routes/users")
 const postRoute = require("./routes/posts")
+const categoriesRoute = require("./routes/categories")
+const multer = require("multer")
 
 dotenv.config()
 
@@ -17,12 +19,23 @@ mongoose
     .then(console.log("Connected to MongoDB"))
     .catch((err)=>console.log(err))
 
-// app.use("/", (req, res)=>{
-//     console.log("main url")
-// })
+const storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+        cb(null, "images");
+    }, filename: (req, file, cb) => {
+        cb(null, req.body.name);
+    },
+    });
+
+const upload = multer({ storage: storage });
+app.post("/api/upload", upload.single("file"), (req,res)=>{
+    res.status(200).json("File has been uploaded");
+})
+
 app.use("/api/auth", authRoute)
 app.use("/api/users", usersRoute)
 app.use("/api/posts", postRoute)
+app.use("/api/categories", categoriesRoute)
 
 app.listen("5000", ()=>{
     console.log("Backend is running")
