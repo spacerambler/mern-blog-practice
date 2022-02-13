d5d77d746d06f6077788c020ab4737f4804e16be
Keely Bailey <keelybailey3@gmail.com>
2022-02-13 13:12:43 -0600

    post routes

--- api/routes/posts.js
+++ api/routes/posts.js
@@ -7,34 +7,85 @@ router.post("/", async (req, res)=> {
     const newPost = new Post(req.body);
     try {
         const savedPost = await newPost.save()
-        res.status(200).json(newPost)
+        res.status(200).json(savedPost)
     } catch(err) {
         res.status(500).json(err)
     }
 })
 
 // UPDATE
-router.put("/:id", async (req,res) =>{
-    if (req.body.userId === req.params.id){
-        if (req.body.password){
-            const salt = await bcrypt.genSalt(10)
-            req.body.password = await bcrypt.hash(req.body.password, salt)
+router.put("/:id", async (req,res) => {
+    try {
+        const post = await Post.findById(req.params.id);
+            if(post.username === req.body.username){
+                try {
+                    const updatedPost = await Post.findByIdAndUpdate(req.params.id, {
+                        $set: req.body,
+                    }, 
+                    {new: true}
+                    );
+                    res.status(200).json(updatedPost);
+                } catch(err) {
+                    res.status(500).json(err);
+                }
+        } else {
+            res.status(401).json("You can only update your own post");
         }
-        try {
-            const updatedUser = await User.findByIdAndUpdate(req.params.id, {
-                $set:req.body,
-            })
-            res.status(200).json(updatedUser)
-        } catch(err) {
-            res.status(500).json(err)
-        }
-    } else {
-        res.status(401).json("Not your account")
+    } catch(err) {
+        res.status(500).json(err);
     }
 })
 
 // DELETE
+router.delete("/:id", async (req,res) => {
+    try {
+        const post = await Post.findById(req.params.id);
+            if(post.username === req.body.username){
+                try {
+                    await post.delete()
+                    res.status(200).json("post has been deleted...");
+                } catch(err) {
+                    res.status(500).json(err);
+                }
+        } else {
+            res.status(401).json("You can only delete your own post");
+        }
+    } catch(err) {
+        res.status(500).json(err);
+    }
+})
 
 // GET 
+router.get("/:id", async (req,res) => {
+    try {
+        const post = await Post.findById(req.params.id);
+        res.status(200).json(post)
+    } catch(err) {
+        res.status(500).json(err)
+    }
+})
+
+// GET ALL
+router.get("/", async (req,res) => {
+    const username = req.query.user;
+    const catName = req.query.cat;
+    try {
+        let posts;
+        if (username){
+            posts = await Post.find({ username })
+        } else if (catName) {
+            posts = await Post.find({ 
+                categories:{
+                $in:[catName],
+            },
+        })
+        } else {
+            posts = await Post.find();
+        }
+        res.status(200).json(posts)
+    } catch(err) {
+        res.status(500).json(err)
+    }
+})
 
 module.exports = router
